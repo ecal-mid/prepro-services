273ec13891510c84d70ba38b61617f669dd38396
Cyril Diagne <cyril.diagne@ecal.ch>
2018-02-25 16:37:22 +0100

    deprecate video2flow

--- deprecated/video2flow/vide2flow_test.py
+++ deprecated/video2flow/vide2flow_test.py
@@ -7,22 +7,23 @@ from protos import bytes2bytes_pb2
 from protos import bytes2bytes_pb2_grpc
 
 parser = argparse.ArgumentParser()
+parser.add_argument('-i', '--input', default='test.mov', help='Input file')
+parser.add_argument('-o', '--output', default='frames.zip', help='Output file')
 parser.add_argument(
-    '-i', '--input', default='_test_data/video_micro.mov', help='Input file')
-parser.add_argument(
-    '-o', '--output', default='_test_data/frames.zip', help='Output file')
-parser.add_argument(
-    '-r', '--rpc_url', default='localhost:50051', help='RPC url')
+    '-r', '--rpc_url', default='10.192.250.140:30003', help='RPC url')
 args = parser.parse_args()
+
+MAX_MESSAGE_LENGTH = 200 * 1024 * 1024
 
 
 def run():
-    channel = grpc.insecure_channel(args.rpc_url)
+    channel = grpc.insecure_channel(
+        args.rpc_url,
+        options=[('grpc.max_receive_message_length', MAX_MESSAGE_LENGTH)])
     stub = bytes2bytes_pb2_grpc.Bytes2BytesStub(channel)
     with open(args.input, "r") as f:
         content = f.read()
         response = stub.Run(bytes2bytes_pb2.Bytes2BytesRequest(input=content))
-        print(response.output)
         with open(args.output, 'w') as fo:
             fo.write(response.output)
 
